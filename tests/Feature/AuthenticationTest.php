81af55c901383852063d5a5224f970eed1e4abae
Washington da Costa Silva <washington3636@gmail.com>
2020-11-01 03:41:20 -0300

    Testes unitarios autenticacao

---
+++ tests/Feature/AuthenticationTest.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class AuthenticationTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_user_can_view_a_login_form()
+    {
+        $response = $this->get('/login');
+
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.login');
+    }
+
+   /* public function test_criarUsuario()
+    {
+        $user = User::factory(100)->create();
+    }*/
+
+    public function test_login(){
+
+        $user = User::factory()->create();
+        
+        $response = $this->from('/login')->post('/login', [
+            'email' => $user->email,
+            'password' => $user->password
+        ]);
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    public function test_user_cannot_view_a_login_form_when_authenticated()
+    {
+        $user = User::factory()->make();
+
+        $response = $this->actingAs($user)->get('/login');
+
+        $response->assertRedirect('/home');
+    }
+
+    public function test_user_cannot_login_with_incorrect_password()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt('i-love-laravel'),
+        ]);
+        
+        $response = $this->from('/login')->post('/login', [
+            'email' => $user->email,
+            'password' => 'invalid',
+        ]);
+        
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+}
