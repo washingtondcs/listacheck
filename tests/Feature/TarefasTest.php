cea7edd4df7f4e4f3934829410ad23de6aa9218e
Washington da Costa Silva <washington3636@gmail.com>
2020-11-03 07:44:55 -0300

    Teste unitarios tarefas

---
+++ tests/Feature/TarefasTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class TarefasTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_usario_ver_home_com_lista_de_tarefas()
+    {
+        $user = User::factory()->create();
+        $url = route('login');
+        $values = [
+            'email' => $user->email,
+            'password' => 'password'
+        ];
+        $this->post($url, $values);
+        $this->assertAuthenticated();
+        
+        $response = $this->get('/home');
+        $response->assertStatus(200);
+
+    }
+
+    public function test_nao_criar_tarefa_sem_usuario_logado(){
+
+        $response = $this->post('novaTarefa', [
+            'titulo' => 'Tarefa Teste',
+            'descricao' => 'Tarefa criada a partir de teste unitário',
+            'prioridade' => 'Alta',
+            'responsavel' => 'PHPUnit',
+            'concluida' => '0'
+        ]);
+
+        $response->assertStatus(500);
+    }
+}
